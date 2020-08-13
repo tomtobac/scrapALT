bdbdc6bbafb4d2b7b3e54037c2a4a9b05fd8e1e1
Jose Pascual <jmpascualjimenez@gmail.com>
2020-08-13 19:17:34 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
+
+const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
+puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
+
+const BASE_URL = "https://alternativeto.net/platform/all/";
+
+const getListInfo = async (page, url) => {
+  return items;
+};
+
+// init
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const maxPage = 5;
+
+  for (let index = 1; index <= maxPage; index++) {
+    let url = `${BASE_URL}?p=${index}`;
+    await page.goto(url);
+    await page.waitFor(2000);
+    const items = await page.evaluate(() =>
+      Array.from(document.querySelectorAll("#mainContent .app-list-item")).map(
+        (element) => {
+          element[
+            Object.keys(element).find((key) =>
+              key.startsWith("__reactInternalInstance")
+            )
+          ].return.pendingProps;
+        }
+      )
+    );
+    console.log(items);
+  }
+
+  let data = JSON.stringify(items, null, 2);
+  fs.writeFileSync("items.json", data);
+
+  await browser.close();
+})();
+
+// const items = await page.evaluate(async () => {
+//   const list = document.querySelectorAll("#mainContent .app-list-item");
+//   return Array.from(list).map((item) => {
+//     const tags = Array.from(item.querySelectorAll(".badges li")).filter(
+//       (tag) => {
+//         console.log(tag.classList.contains === "badge-license");
+//         return tag;
+//       }
+//     );
+
+//     return {
+//       name: item.querySelector(".app-info .app-header h2 a").innerText,
+//       link: item
+//         .querySelector(".app-info .app-header h2 a")
+//         .getAttribute("href"),
+//       description: item.querySelector(".app-info .description p").innerText,
+//       avatar: item.querySelector(".mobile-icon img").getAttribute("src"),
+//       votes: item.querySelector(".like-button > span").innerText,
+//       openSource: item
+//         .querySelector(".badges .badge-license")
+//         .innerText.toLowerCase()
+//         .includes("open source"),
+//       badge: item.querySelector(".badges .badge-license").innerText,
+//       tags,
+//     };
+//   });
